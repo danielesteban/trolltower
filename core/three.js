fcd1354c303ed62e2443f67740640f8b1f50b461
Daniel Esteban Nombela <dani@gatunes.com>
2020-11-26 04:24:22 +0100

    Bump three revision to 0.123.0

--- core/three.js
+++ core/three.js
@@ -1,4 +1,4 @@
-export * from 'https://cdn.jsdelivr.net/npm/three@0.122.0/build/three.module.js';
-export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/utils/BufferGeometryUtils.js';
-export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/loaders/GLTFLoader.js';
-export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.122.0/examples/jsm/math/SimplexNoise.js';
+export * from 'https://cdn.jsdelivr.net/npm/three@0.123.0/build/three.module.js';
+export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.123.0/examples/jsm/utils/BufferGeometryUtils.js';
+export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.123.0/examples/jsm/loaders/GLTFLoader.js';
+export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.123.0/examples/jsm/math/SimplexNoise.js';
