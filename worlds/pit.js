0226007f76a901e43494b01d216856e1e915d3c5
Daniel Esteban Nombela <dani@gatunes.com>
2020-12-18 15:05:47 +0100

    Refactor lava to be able to have multiply instances and integrate it into the gameplay class

--- worlds/pit.js
+++ worlds/pit.js
@@ -1,6 +1,5 @@
 import { Color, FogExp2, Vector3 } from '../core/three.js';
 import Gameplay from '../core/gameplay.js';
-import Lava from '../renderables/lava.js';
 
 class Pit extends Gameplay {
   constructor(scene, { instance, offset, spectator }) {
@@ -19,6 +18,11 @@ class Pit extends Gameplay {
       elevators: [
         { position: new Vector3(0, 2.5, -31.75), rotation: 0 },
         { position: new Vector3(0, 2.5, 31.75), rotation: Math.PI },
+      ],
+      lava: [
+        { position: new Vector3(0, 0.25, -22), width: 16, depth: 20 },
+        { position: new Vector3(0, 0.25, 0), width: 16, depth: 24 },
+        { position: new Vector3(0, 0.25, 22), width: 16, depth: 20 },
       ],
       platforms: {
         instances: [...Array(38)].map((v, i) => {
@@ -50,52 +54,14 @@ class Pit extends Gameplay {
       terrainPhysics: 'models/pitPhysics.json',
     });
 
-    const { models, sfx } = scene;
-
     scene.background = new Color(0x220011);
     scene.fog = new FogExp2(scene.background.getHex(), 0.02);
-
-    const lava = new Lava({ width: 16, depth: 64, sfx });
-    this.add(lava);
-    this.lava = lava;
-
-    models.load('models/pit.glb')
+    scene.models.load('models/pit.glb')
       .then((model) => {
         model.scale.setScalar(0.5);
         this.add(model);
         this.spawn.isOpen = true;
       });
-
-    scene.getPhysics()
-      .then(() => {
-        lava.onContact = this.projectiles.destroyOnContact;
-        this.physics.addMesh(lava, 0, { isTrigger: true });
-      });
-  }
-
-  onAnimationTick(animation) {
-    const { effects, player } = this;
-    super.onAnimationTick(animation);
-    Lava.animate(animation);
-    if (player.head.position.y < 3) {
-      effects.burning.trigger();
-      player.controllers.forEach((controller) => {
-        if (controller.hand) {
-          controller.pulse(0.4, 10);
-        }
-      });
-    }
-  }
-
-  resumeAudio() {
-    const { lava } = this;
-    lava.resumeAudio();
-  }
-
-  onUnload() {
-    const { lava } = this;
-    super.onUnload();
-    lava.stopAudio();
   }
 }
 
