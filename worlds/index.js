7f1e801471add20774f8fa36a853099e91685a11
Daniel Esteban Nombela <dani@gatunes.com>
2020-12-18 21:00:41 +0100

    The Well + The Lava Pit === The Well v2

--- worlds/index.js
+++ worlds/index.js
@@ -1,5 +1,4 @@
 export { default as Brittle } from './brittle.js';
 export { default as Menu } from './menu.js';
-export { default as Pit } from './pit.js';
 export { default as Tower } from './tower.js';
 export { default as Well } from './well.js';
