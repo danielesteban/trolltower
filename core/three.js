ca06acf250dd3360a3e728c8fdfde97069257c2a
Daniel Esteban Nombela <dani@gatunes.com>
2021-01-27 12:05:23 +0100

    Upgrade three to rev 125

--- core/three.js
+++ core/three.js
@@ -1,4 +1,4 @@
-export * from 'https://cdn.jsdelivr.net/npm/three@0.124.0/build/three.module.js';
-export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/utils/BufferGeometryUtils.js';
-export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/loaders/GLTFLoader.js';
-export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.124.0/examples/jsm/math/SimplexNoise.js';
+export * from 'https://cdn.jsdelivr.net/npm/three@0.125.1/build/three.module.js';
+export { BufferGeometryUtils } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/utils/BufferGeometryUtils.js';
+export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/loaders/GLTFLoader.js';
+export { SimplexNoise } from 'https://cdn.jsdelivr.net/npm/three@0.125.1/examples/jsm/math/SimplexNoise.js';
